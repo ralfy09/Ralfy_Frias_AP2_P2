4fc7174b2507d1733f55e14dc721a53669130c23
Ralfy <ralfyfrias@gmail.com>
2026-03-11 20:27:29 -0400

    agregando los use case

---
+++ app/src/main/java/com/example/ralfy_frias_ap2_p2/domain/usecase/ActualizarJugadorUseCase.kt
@@ -0,0 +1,1 @@
+package com.example.ralfy_frias_ap2_p2.domain.usecase
---
+++ app/src/main/java/com/example/ralfy_frias_ap2_p2/domain/usecase/GetJugadorIdUsecase.kt
@@ -0,0 +1,5 @@
+package com.example.ralfy_frias_ap2_p2.domain.usecase
+
+import com.example.ralfy_frias_ap2_p2.data.remote.Dto.JugadorResponseDTO
+import com.example.ralfy_frias_ap2_p2.data.remote.remotedatasource.JugadorRemoteDataSource
+
